e08abfa00ebd5ea1d6cb623118e836f0bd4dc7b5
Aurelijus <aurelijus.posius@gmail.com>
2020-01-29 10:59:21 +0200

    Created basic invoice

--- app/Http/Controllers/ProfileController.php
+++ app/Http/Controllers/ProfileController.php
@@ -6,6 +6,9 @@ namespace App\Http\Controllers;
 use App\Core\Views\View;
 use App\Course;
 use App\Order;
+use Mpdf\HTMLParserMode;
+use Mpdf\Mpdf;
+use Mpdf\Output\Destination;
 use Symfony\Component\HttpFoundation\Request;
 
 use App\User;
@@ -93,5 +96,55 @@ class ProfileController extends Controller
         $purchases = Order::select('orders.*, courses.name')->where('user_id', '=', $user->id)->join('courses', 'courses.id', '=', 'orders.course_id')->groupBy('orders.id')->all();
 
         return view('profile/purchases', ['purchases' => $purchases]);
+    }
+
+    public function purchaseShow(Request $request, Mpdf $mpdf, $id)
+    {
+        $user = $request->getSession()->get('user');
+
+        $order = Order::find($id);
+
+        if (!empty($order) && $order->user_id == $user->id) {
+
+            $order->course = Course::find($order->course_id);
+
+            $style = '
+
+';
+
+            $body = '
+<h1 style="text-align: center;">Sąskaitos faktūra</h1>
+<table width="100%" style="margin-top: 25px; font-family: serif;" cellpadding="10">
+    <tr>
+        <td width="45%" style="border: 0.1mm solid #888888; "><span style="font-size: 14pt; font-weight: bold; font-family: sans;">Pardavėjas:</span><br /><br />Internetiniai kursai<br />Ozo g. 25<br />+370 612 34567</td>
+        <td width="10%">&nbsp;</td>
+        <td width="45%" style="border: 0.1mm solid #888888;"><span style="font-size: 14pt; font-weight: bold; font-family: sans;">Pirkėjas:</span><br /><br />' . $user->email . '<br />...</td>
+    </tr>
+</table>
+<table width="100%" style="margin-top: 50px; font-family: serif; border-collapse: collapse;" cellpadding="10">
+    <tr>
+        <th style="border: 1px solid black;">Eil. Nr.</th>
+        <th style="border: 1px solid black;">Prekės, paslaugos pavadinimas</th>
+        <th style="border: 1px solid black;">Kaina</th>
+    </tr>
+    <tr>
+        <td style="border: 1px solid black;">'. $order->id .'</td>
+        <td style="border: 1px solid black;">'. $order->course->name .'</td>
+        <td style="border: 1px solid black; text-align: right;">'. $order->price .'</td>
+    </tr>
+    <tr>
+        <td style="border: 1px solid black; font-weight: bold; text-align: right;" colspan="2">Viso:</td>
+        <td style="border: 1px solid black; font-weight: bold; text-align: right;">' . $order->price . '</td>
+    </tr>
+</table>
+';
+
+            $mpdf->SetTitle('Sąskaitos faktūra');
+            $mpdf->WriteHTML($style, HTMLParserMode::HEADER_CSS);
+            $mpdf->WriteHTML($body, HTMLParserMode::HTML_BODY);
+            $mpdf->Output('saskaitos-faktura', Destination::INLINE);
+        } else {
+            return view('errors/error404');
+        }
     }
 }
