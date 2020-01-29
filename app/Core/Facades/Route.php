e2ea3d695094662e923ee705279684990de1b43d
Aurelijus <aurelijus.posius@gmail.com>
2020-01-29 10:08:21 +0200

    Added Route facade

--- app/Core/Facades/Route.php
+++ app/Core/Facades/Route.php
@@ -7,32 +7,17 @@ namespace App\Core\Facades;
 /**
  * Class Route
  * @package App\Core\Facades
+ *
+ * @method static \App\Core\Routing\Route get(string $url, $controller)
+ * @method static \App\Core\Routing\Route post(string $url, $controller)
+ * @method static \App\Core\Routing\Route put(string $url, $controller)
+ * @method static \App\Core\Routing\Route patch(string $url, $controller)
+ * @method static \App\Core\Routing\Route delete(string $url, $controller)
  */
-class Route
+class Route extends Facade
 {
-    public static function get(string $uri, string $action)
+    protected static function getFacadeAccessor(): string
     {
-
+        return 'router';
     }
-
-    public static function post(string $uri, string $action)
-    {
-
-    }
-
-    public static function put(string $uri, string $action)
-    {
-
-    }
-
-    public static function patch(string $uri, string $action)
-    {
-
-    }
-
-    public static function delete(string $uri, string $action)
-    {
-
-    }
-
 }
